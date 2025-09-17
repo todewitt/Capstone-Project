14f2f4081e19ff0d485ca78f2ca1155f866fb54b
todewitt <todewitt@asu.edu>
2025-09-17 12:03:26 -0700

    added new routes
    added new routes for account options

--- app.py
+++ app.py
@@ -4,12 +4,34 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template(home.html)
+    return render_template("home.html")
 
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
 
 @app.route("/account")
 def account():
-    return render_template(account.html)
+    return render_template("account.html")
+
+@app.route("/log-out")
+def logout():
+    return render_template("log-out.html")
+
+@app.route("/order-history")
+def orderHistory():
+    return render_template("order-history.html")
+
+@app.route("/withdraw-deposit.html")
+def withdrawDeposit():
+    return render_template("withdraw-deposit.html")
+
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
