061baad283ca72f9265d9eb5a7b9ff56fd494595
todewitt <todewitt@asu.edu>
2025-09-17 10:19:01 -0700

    app.py
    added app.py

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template(home.html)
+
+
+@app.route("/account")
+def account():
+    return render_template(account.html)
+
+if __name__ == "__main__":
+    app.run(debug=True)
+    
