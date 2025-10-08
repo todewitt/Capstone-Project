cb82e20d824cd43f145279269b4413b4312b66e7
todewitt <todewitt@asu.edu>
2025-10-08 16:19:06 -0700

    Dynamic Market hours and schedule
    Made market hours dynamic and allows admins to change it in the new admin dashboard

--- WorkingDir/app.py
+++ WorkingDir/app.py
@@ -47,22 +47,45 @@ class Stock(db.Model):
 
     def __repr__(self):
         return f"<Stock {self.stock_symbol}>"
-    
+
+class MarketSchedule(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    # 0 = monday 6 = sunday 
+    day_of_week = db.Column(db.Integer, primary_key=True, unique=True, nullable=False)
+    open_time = db.Column(db.Time, nullable=False)
+    close_time = db.Column(db.Time, nullable=False)
+
+class MarketOverride(db.Model):
+    id = db.Column(db.Integer, primary_key=True, unique=True, nullable=False)
+    override_date = db.Column(db.Date, unique=True, nullable=False)
+    open_time = db.Column(db.Time, nullable=False)
+    close_time = db.Column(db.Time, nullable=False)
+
 # Create tables
 with app.app_context():
     db.create_all()
 
 # Function sets a time zone, defines when Market starts and ends. 
 def is_market_open():
-    tz = pytz.timezone('US\Eastern')
-    time_now = datetime.datetime.now(tz)
-    start_market = now.replace(hour=9, minute=0, second=0, microsecond=0)
-    end_market = now.replace(hour=16, minute=0, second=0, microsecond=0)
-# Checks if current time is within the Weekdays and within 9am to 4pm
-    if 0 <= now.weekday() <= 4 and start_market <= now <= end_market:
-        return True
-    return False    
-# ROUTES
+        tz = pytz.timezone('US/Eastern')
+        now = datetime.datetime.now(tz)
+        today_date = now.date()
+        override = MarketOverride.query.filter_by(override_date=today_date).first()
+        if override:
+            start_market = now.replace(hour=override.open_time.hour, minute=override.open_time.minute, second=0)
+            end_market = now.replace(hour=override.close_time.hour, minute=override.close_time.minute, second=0)
+            if override.open_time == override.close_time:
+                return False
+            return start_market <= now <= end_market
+        
+        weekday = now.weekday() # 0 = monday 
+        schedule = MarketSchedule.query.filter_by(day_of_week=weekday).first()
+        if not schedule:
+            return False
+        start_market = now.replace(hour=schedule.open_time.hour, minute=schedule.open_time.minute, second=0)
+        end_market = now.replace(hour=schedule.close_time.hour, minute=schedule.close_time.minute, second=0)
+        return start_market <= now <= end_market 
+
 @app.route("/", methods=['GET', 'POST'])
 def login():
     #Check if user sumitted something and store the username and password provided by user then quesries User to find the
@@ -97,7 +120,7 @@ def buy_sell():
 def buy():
     if not is_market_open():
         flash('The Market is closed at the moment.' \
-        'Plese Try your Purchase again during Monday - Friday from 9am - 4pm.')
+        'Please Try again later.')
         return redirect(url_for('buy_sell'))
     stock_symbol = request.form['stock_symbol'].upper()
     quantity = int(request.form['quantity'])
@@ -145,7 +168,7 @@ def sell():
     if not is_market_open():
 
         flash('The Market is closed at the moment.' \
-        'Plese Try your Se again during Monday - Friday from 9am - 4pm.')
+        'Plese Try again later.')
         return redirect(url_for('buy_sell'))
     stock_symbol = request.form['stock_symbol'].upper()
     quantity = int(request.form['quantity'])
@@ -276,8 +299,8 @@ def create_account():
     
     return render_template('create-account.html')
 
-@app.route("/edit-stocks", methods=['GET', 'POST'])
-def edit_stocks():
+@app.route("/admin-dashboard", methods=['GET', 'POST'])
+def admin_dashboard():
     user_id = session.get('user_id')
     if not user_id:
         flash('Please log in with an admin account.', 'error')
@@ -288,32 +311,75 @@ def edit_stocks():
         flash('User is not logged in as an admin.', 'error')
         return redirect(url_for('dashboard'))
     
+    tz = pytz.timezone('US/Eastern')
+    today_date = datetime.datetime.now(tz).date()
+
     if request.method == 'POST':
-        stock_symbol = request.form['stock_symbol'].upper()
-        name = request.form['name']
-        price_per_share = request.form['price_per_share']
-        quantity = request.form['quantity']
+        if 'add_stock' in request.form:
+            stock_symbol = request.form['stock_symbol'].upper()
+            name = request.form['name']
+            price_per_share = request.form['price_per_share']
+            quantity = request.form['quantity']
         
-        if not stock_symbol or not name or not price_per_share or not quantity:
-            flash('Please fill in all fields', 'error')
-            return redirect(url_for('edit_stocks'))
-        
-        try:
-            new_stock = Stock(
-                stock_symbol=stock_symbol, 
-                name=name, 
-                price_per_share=float(price_per_share), 
-                quantity=float(quantity)
-            )
-            db.session.add(new_stock)
-            db.session.commit()
-            flash('Stock created successfully!', 'success')
-            return redirect(url_for('edit_stocks'))
-        except Exception as e:
-            flash(f'Error creating stock: {str(e)}', 'error')
-            return redirect(url_for('edit_stocks'))
-    
-    return render_template('edit-stocks.html')
+            if not stock_symbol or not name or not price_per_share or not quantity:
+                flash('Please fill in all fields', 'error')
+                return redirect(url_for('admin_dashboard'))
+            
+            try:
+                new_stock = Stock(
+                    stock_symbol=stock_symbol, 
+                    name=name, 
+                    price_per_share=float(price_per_share), 
+                    quantity=float(quantity)
+                )
+                db.session.add(new_stock)
+                db.session.commit()
+                flash('Stock created successfully!', 'success')
+                return redirect(url_for('admin_dashboard'))
+            except Exception as e:
+                flash(f'Error creating stock: {str(e)}', 'error')
+                return redirect(url_for('admin_dashboard'))
+        elif 'update_schedule' in request.form:
+            for day in range(7):
+                open_time_str = request.form.get(f'open_time_{day}')
+                close_time_str = request.form.get(f'close_time_{day}')
+                is_open = request.form.get(f'is_open_{day}')
+                schedule = MarketSchedule.query.filter_by(day_of_week=day).first()
+                if not schedule:
+                    schedule = MarketSchedule(day_of_week=day)
+                    db.session.add(schedule)
+                if is_open:
+                    schedule.open_time = datetime.datetime.strptime(open_time_str, '%H:%M').time()
+                    schedule.close_time = datetime.datetime.strptime(close_time_str, '%H:%M').time()
+                else:
+                    schedule.open_time = datetime.time(0, 0)
+                    schedule.close_time = datetime.time(0 , 0)
+            flash('market schedule updated', 'success')
+        elif 'manual_override' in request.form:
+            open_time_str = request.form.get('override_open_time')
+            close_time_str = request.form.get('override_close_time')
+            is_closed_today = request.form.get('is_closed_today')
+            override = MarketOverride.query.filter_by(override_date=today_date).first()
+            if not override:
+                override = MarketOverride(override_date=today_date)
+                db.session.add(override)
+            if is_closed_today:
+                override.open_time = datetime.time(0, 0)
+                override.close_time = datetime.time(0, 0)
+                flash('Market has been closed.', 'success')
+            else:
+                override.open_time = datetime.datetime.strptime(open_time_str, '%H:%M').time()
+                override.close_time = datetime.datetime.strptime(close_time_str, '%H:%M').time()
+                flash('Regular hours have been overridden')
+
+        db.session.commit()
+        return redirect(url_for('admin_dashboard'))
+    schedules = MarketSchedule.query.all()
+    schedule_dict = {schedule.day_of_week: schedule for schedule in schedules}
+    days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+    todays_override = MarketOverride.query.filter_by(override_date=today_date).first()
+
+    return render_template('admin-dashboard.html', schedule_dict=schedule_dict, days=days, todays_override=todays_override, today_string=today_date.strftime('%A, %B %d'))
 
 if __name__ == '__main__':
     app.run(debug=True)
